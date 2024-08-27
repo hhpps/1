4a4ac42058c81fc0bc0b577115f8f4bc816fe55d
hhpps <91615031+hhpps@users.noreply.github.com>
2024-08-28 02:44:11 +0900

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-document.querySelector('.nav-menu button').addEventListener('click', () => {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-});
-
