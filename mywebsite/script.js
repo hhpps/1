840b873aaa22d39935b05be7e9f0db1e294e8a4f
hhpps <91615031+hhpps@users.noreply.github.com>
2024-08-27 12:21:40 +0900

    Add files via upload

---
+++ mywebsite/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.querySelector('.nav-menu button').addEventListener('click', () => {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+});
